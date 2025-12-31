7379c5475b8a88c72d403d57c344d0a3a684b26f
Vyshnavi <vyshnavivgowda@gmail.com>
2025-12-31 16:10:23 +0530

    Fix: White screen fix and forced icon update

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  output: 'export',
+  assetPrefix: './',     // This is the fix for the white screen
   trailingSlash: true,
   images: {
     unoptimized: true,
